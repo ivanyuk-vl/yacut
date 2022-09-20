d80ac831e86ca56f1cfd7927ce72c090e6d94893
Влад Иванюк <ivanyuk.vl@yandex.ru>
2022-09-20 23:09:35 +1000

    Fix template

--- yacut/forms.py
+++ yacut/forms.py
@@ -1,13 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, URLField
-from wtforms.validators import DataRequired, Length, Optional
+from wtforms.validators import URL, DataRequired, Length, Optional
 
-from .settings import (MAX_URL_ID_LENGTH, MAX_URL_LENGTH, MIN_URL_ID_LENGTH,
-                       MIN_URL_LENGHT)
+from .settings import MAX_URL_ID_LENGTH, MAX_URL_LENGTH
 
 LINK_LABEL = 'Длинная ссылка'
-LINK_ID = 'form-title'
-REQUIRED_FIELD_MESSAGE = 'Обязательное поле'
+ID_FOR_LINK = 'form-title'
 ID_LABEL = 'Ваш вариант короткой ссылки'
 ID_FOR_ID = 'form-link'
 
@@ -16,13 +14,14 @@ class URLForm(FlaskForm):
     original_link = URLField(
         LINK_LABEL,
         (
-            DataRequired(REQUIRED_FIELD_MESSAGE),
-            Length(MIN_URL_LENGHT, MAX_URL_LENGTH),
+            DataRequired(),
+            Length(max=MAX_URL_LENGTH),
+            URL(),
         ),
-        id=LINK_ID,
+        id=ID_FOR_LINK,
     )
     custom_id = StringField(
         ID_LABEL,
-        (Optional(), Length(MIN_URL_ID_LENGTH, MAX_URL_ID_LENGTH)),
+        (Optional(), Length(max=MAX_URL_ID_LENGTH)),
         id=ID_FOR_ID,
     )
