54e76c65b777a8657db9beb802035b4eed2e848e
Влад Иванюк <ivanyuk.vl@yandex.ru>
2022-09-24 22:23:53 +1000

    Update README.md

--- yacut/__init__.py
+++ yacut/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_babel import Babel
-from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 from settings import Config
@@ -11,6 +10,5 @@ app.url_map.strict_slashes = False
 babel = Babel(app)
 babel.localeselector(lambda: app.config.get('LANGUAGE'))
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 
 from . import api_views, cli_commands, models, views
--- yacut/cli_commands.py
+++ yacut/cli_commands.py
@@ -1,7 +1,7 @@
 from . import app, db
 
 
-@app.cli.command('create_table')
+@app.cli.command('create_all')
 def create_all_command():
     """Создать таблицу в базе данных."""
     db.create_all()
